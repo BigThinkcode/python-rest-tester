3fa3d78d6299d7eba56106f4bd65aabceb220c2c
naveenkumar86420 <naveenkumar.m@bigthinkcode.com>
2024-07-25 17:27:20 +0530

    Add PR changes

--- rest_tester/configs/configs.py
+++ rest_tester/configs/configs.py
@@ -40,13 +40,13 @@ configs = {
         },
         {
             # Token for the user
-            "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwidXNlcm5hbWUiOiJlbWlseXMiLCJlbWFpbCI6ImVtaWx5LmpvaG5zb25AeC5kdW1teWpzb24uY29tIiwiZmlyc3ROYW1lIjoiRW1pbHkiLCJsYXN0TmFtZSI6IkpvaG5zb24iLCJnZW5kZXIiOiJmZW1hbGUiLCJpbWFnZSI6Imh0dHBzOi8vZHVtbXlqc29uLmNvbS9pY29uL2VtaWx5cy8xMjgiLCJpYXQiOjE3MjAxMzE1NTUsImV4cCI6MTcyMDEzNTE1NX0.UeOhe-VgV8ojCgPgV8mqlJne18fubOuziPMRJxjkf8A",
+            "token": "<place token here>",
             # List of test groups for the user
             "test_groups": ["group2/"]
         },
         {
             # Token for the user
-            "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NCwidXNlcm5hbWUiOiJqYW1lc2QiLCJlbWFpbCI6ImphbWVzLmRhdmlzQHguZHVtbXlqc29uLmNvbSIsImZpcnN0TmFtZSI6IkphbWVzIiwibGFzdE5hbWUiOiJEYXZpcyIsImdlbmRlciI6Im1hbGUiLCJpbWFnZSI6Imh0dHBzOi8vZHVtbXlqc29uLmNvbS9pY29uL2phbWVzZC8xMjgiLCJpYXQiOjE3MjAxMzE1NzksImV4cCI6MTcyMDEzNTE3OX0.QL7GISIhcyYg8hEAb7rGTsp3FcELnWrhFgvp9XuO8dQ",
+            "token": "<place token here>",
             # List of test groups for the user
             "test_groups": ["group3/"]
         }
